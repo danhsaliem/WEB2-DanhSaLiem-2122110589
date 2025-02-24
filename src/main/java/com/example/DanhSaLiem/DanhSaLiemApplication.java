e4cffdfb077861bb79b6b388e1b085fda5f747d8
danhsaliem <danhliem2004@gmail.com>
2025-02-24 10:31:58 +0700

    commit

---
+++ src/main/java/com/example/DanhSaLiem/DanhSaLiemApplication.java
@@ -0,0 +1,13 @@
+package com.example.DanhSaLiem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanhSaLiemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanhSaLiemApplication.class, args);
+	}
+
+}
